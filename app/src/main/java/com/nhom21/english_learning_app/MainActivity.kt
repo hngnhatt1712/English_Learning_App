3a79e5c7ab53be3bb6b54e8f99cc888ca6ffe8bf
hngnhat1712 <daonhat1712@gmail.com>
2026-09-19 00:26:25 +0700

    Setup project architecture

---
+++ app/src/main/java/com/nhom21/english_learning_app/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.nhom21.english_learning_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.nhom21.english_learning_app.ui.theme.English_Learning_AppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            English_Learning_AppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    English_Learning_AppTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/nhom21/english_learning_app/features/auth/loginViewModel.kt
@@ -0,0 +1,2 @@
+package com.nhom21.english_learning_app.features.auth
+
